c867d521334f9029a008121887cab7267bde5605
ARMAzac <armazac@gmail.com>
2016-10-08 07:11:35 +1100

    removed json serialization in db

--- lib.py
+++ lib.py
@@ -1,4 +1,5 @@
 import asyncio,json,os
+from datetime import date, timedelta
 import motor.motor_asyncio
 from simplekv.fs import FilesystemStore
 
@@ -53,7 +54,7 @@ class PersistentPlugin(Plugin):
         if not data:
             data = {'plugin':self.__class__.__name__}
         for name in self.persist:
-            val = json.dumps(getattr(self,name))
+            val = getattr(self,name)
             data[name] = val
         yield from coll.save(data)
 
@@ -67,4 +68,44 @@ class PersistentPlugin(Plugin):
                     val = data[name]
                 except:
                     continue
-                setattr(self, name, json.loads(val))
+                setattr(self, name, val)
+
+class TimedPersistentPlugin(Plugin):
+    persist = []
+
+    @asyncio.coroutine
+    def save(self):
+        coll = store[self.__class__.__name__]
+        data = yield from coll.find_one({'date': date.today().isoformat()})
+        if not data:
+            data = {'plugin': self.__class__.__name__}
+        for name in self.persist:
+            val = getattr(self, name)
+            data[name] = val
+        yield from coll.save(data)
+
+    @asyncio.coroutine
+    def load(self):
+        coll = store[self.__class__.__name__]
+        data = yield from coll.find_one({'date': date.today().isoformat()})
+        if data:
+            for name in self.persist:
+                try:
+                    val = data[name]
+                except:
+                    continue
+                setattr(self, name, val)
+
+    def sum_week(self, attr):
+        coll = store[self.__class__.__name__]
+        ret = {}
+        d = date.today() - timedelta(days=7)
+        for x in range(1,7):
+            data = yield from coll.find_one({'date': date.today().isoformat()})
+            for k,v in data[attr].items():
+                if isinstance(v,int):
+                    if k not in ret:
+                        ret[k] = v
+                    else:
+                        ret[k] += v
+
