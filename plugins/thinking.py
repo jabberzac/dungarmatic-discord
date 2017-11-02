660659ff326e37671ce45e4ca6d6a14e18e49f26
ARMAzac <armazac@gmail.com>
2017-11-02 19:27:18 +1100

    thinking emoji reacts

---
+++ plugins/thinking.py
@@ -0,0 +1,18 @@
+from lib import Plugin
+import markovify, feedparser, random
+from datetime import datetime, timedelta
+from bson import Code
+import asyncio
+from tornado import gen
+import re, html
+
+
+class ThinkingEmojiPlugin(Plugin):
+
+    @asyncio.coroutine
+    def on_message(self, message, history):
+        if message.content.find("🤔") > -1:
+            yield from self.client.add_reaction(message,'🤔')
+        if message.content.find("<:thinkingbig:326671061225832448>") > -1:
+            yield from self.client.add_reaction(message, "thinkingbig:326671061225832448")
+
